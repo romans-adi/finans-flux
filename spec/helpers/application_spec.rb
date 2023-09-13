d1bd9f08310932fc719c046b4e6947e67a0b3fae
Romans S <romans.spilaks@gmail.com>
2023-09-14 01:57:43 +0300

    Update lists in views to show the full scrollable list and index pages. Add db seeds sample. Update icons spec.

--- spec/helpers/application_spec.rb
+++ spec/helpers/application_spec.rb
@@ -3,18 +3,15 @@ require 'rails_helper'
 RSpec.feature 'Category icon helper', type: :feature do
   include LoginHelpers
   let(:user) { create(:user) }
-  let(:category) { create(:category, author: user) }
-  let(:movement) { create(:movement) }
 
   before do
     log_in_user(user)
-    visit category_movements_path(category)
   end
 
   scenario 'Displays the correct icon for a category with a valid icon' do
-    category = create(:category, icon: 'shopping', name: 'Sample Category 4', author: user)
+    category = create(:category, name: 'shopping', icon: 'fa-shopping-cart', author: user)
     create_list(:movement, 1, category:)
-    visit category_movements_path(category)
-    expect(page).to have_selector('.fas.fa-shopping-cart.text-xl.bg-second.p-4.text-white.rounded-lg')
+    visit categories_path
+    expect(page).to have_css('i.fas.fa-shopping-cart.text-xl.bg-second.p-4.text-white.rounded-lg')
   end
 end
